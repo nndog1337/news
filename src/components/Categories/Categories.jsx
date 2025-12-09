c6d06872dca37ee621784f1ca1bab88399c75d37
Ilya Lukyanov <lukyanov776@gmail.com>
2025-12-09 12:09:29 +0300

    add filtering by categories

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { formatData } from '../../assets/helpers/formatData'
+import styles from "./style.module.css"
+
+function Categories({categories, setSelectedCategory, selectedCategory}) {
+  return (
+    <div className={styles.categories}>
+      {categories.map(category => {
+        return (
+          <button onClick={() => setSelectedCategory(category)} className={selectedCategory === category ? styles.active : styles.item} key={category}>
+            {category}
+          </button>
+        )
+      } )}
+    </div>
+  )
+}
+
+export default Categories
